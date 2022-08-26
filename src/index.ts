1fe8d83246233facb5f73e4dcb6803374378b75c
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-08-26 12:33:29 +0900

    Fix: exit successfully on occurring error

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ export async function lock<T>(locker:LockObj, fn:()=>T){
   if(!(locker instanceof LockObj)) throw new Error("invalid locker was provided")
   if(locker["hasLocked"]) await locker["mutexProbe"]();
   locker["hasLocked"] = true;
-  const result = await fn();
-  locker["hasLocked"] = false;
-  locker["finishHanders"].shift()?.();
-  return result;
+  try{
+    return await fn();
+  }
+  finally{
+    locker["hasLocked"] = false;
+    locker["finishHanders"].shift()?.();
+  }
 }
